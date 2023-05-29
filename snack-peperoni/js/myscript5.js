f86cb2fe7de94dc86461d08795c35ca94bf5c851
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-05-29 12:04:52 +0200

    array created

---
+++ snack-peperoni/js/myscript5.js
@@ -0,0 +1,92 @@
+
+    //Crea un array di 10 oggetti che rappresentano un peperone, indicando per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutti i peperoni.
+
+//Bonus:
+//crea un peperone con una funzione
+
+const peperoni = [
+
+    {
+        varietà : "rosso",
+        peso : 200,
+        lunghezza: 10,
+
+    },
+    {
+        varietà : "verde",
+        peso : 250,
+        lunghezza: 10,
+
+    },
+
+    {
+        varietà : "rosso",
+        peso : 300,
+        lunghezza: 10,
+
+    },
+
+    {
+        varietà : "verde",
+        peso : 232,
+        lunghezza: 10,
+
+    },
+    {
+        varietà : "rosso",
+        peso : 209,
+        lunghezza: 10,
+
+    },
+
+    {
+        varietà : "verde",
+        peso : 129,
+        lunghezza: 13,
+
+    },
+
+    {
+        varietà : "rosso",
+        peso : 200,
+        lunghezza: 9,
+
+    },
+
+    {
+        varietà : "arancione",
+        peso : 345,
+        lunghezza: 10,
+
+    },
+
+    {
+        varietà : "rosso",
+        peso : 500,
+        lunghezza: 11,
+
+    },
+    {
+        varietà : "verde",
+        peso : 320,
+        lunghezza: 13,
+
+    },
+
+
+    
+
+
+  
+
+
+
+
+
+
+
+
+]
+
+
+console.log(peperoni)
